cd2235c58eeaa80758b040625d10c740608a0f55
Marco Silva <marco.silvaponce10@gmail.com>
2025-05-24 00:57:43 -0400

    avance bases

---
+++ src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+@Component({
+    
+    templateUrl: './counter-page.component.html',
+    styleUrls: ['./counter-page.component.css']
+})
+
+export class CounterPageComponent {
+    public counter: number = 10;
+
+    increaseBy(value: number): void {
+        this.counter += value;
+    }
+
+    resetCounter(): void {
+        this.counter = 10;
+    }
+}

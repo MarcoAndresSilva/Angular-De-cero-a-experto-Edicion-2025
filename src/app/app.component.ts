a26400b455d88ad499afdfd380e1b06d60e4a817
Marco Silva <marco.silvaponce10@gmail.com>
2025-06-02 23:25:53 -0400

    add routerlink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { NavbarComponent } from "./components/shared/navbar/navbar.component";
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  // styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Marco Andres';

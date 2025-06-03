473f08b5924a6c9956ee5cb14310fbeabbcf7853
Marco Silva <marco.silvaponce10@gmail.com>
2025-06-03 07:28:48 -0400

    dragonball-page preparar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { DragonballComponent } from './pages/dragonball/dragonball-page.component';
 
 export const routes: Routes = [
     {
@@ -11,9 +13,14 @@ export const routes: Routes = [
         path: 'hero',
         component: HeroPageComponent,
     },
+     {
+        path: 'dragonball',
+        component: DragonballComponent,
+    },
     {
         path: '**',
         redirectTo: '',
     },
-
+   
 ];
+

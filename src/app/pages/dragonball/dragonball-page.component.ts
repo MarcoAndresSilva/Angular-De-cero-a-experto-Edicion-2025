9b7c98baa32d1759a7542df34e04135aad22a920
Marco Silva <marco.silvaponce10@gmail.com>
2025-06-07 02:17:11 -0400

    Inputs manejo de datos update y señales

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -17,17 +17,17 @@ interface Character {
 })
 export class DragonballPageComponent {
 
-  name = signal('Goten');
-  power = signal(100);
+  name = signal('');
+  power = signal(0);
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 10000 },
     { id: 2, name: 'Vegeta', power: 9500 },
     { id: 3, name: 'Gohan', power: 7500 },
-    { id: 4, name: 'Piccolo', power: 7000 },
-    { id: 5, name: 'Krillin', power: 5000 },
-    { id: 6, name: 'Yamcha', power: 4000 },
-    { id: 7, name: 'Trunks', power: 3000 },
+    // { id: 4, name: 'Piccolo', power: 7000 },
+    // { id: 5, name: 'Krillin', power: 5000 },
+    // { id: 6, name: 'Yamcha', power: 4000 },
+    // { id: 7, name: 'Trunks', power: 3000 },
     // { id: 8, name: 'Nappa', power: 2000 },
     // { id: 9, name: 'Broly', power: 1000 },
     // { id: 10, name: 'Goten', power: 100 },
@@ -41,15 +41,21 @@ export class DragonballPageComponent {
   // });
 
   addCharacter(): void {
-    this.characters.set([
-      ...this.characters(),
-      {
-        id: this.characters().length + 1,
-        name: this.name(),
-        power: this.power(),
-      },
-    ]);
+    if(!this.name() || !this.power() || this.power() <= 0) return;
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    this.characters.update((list) => [...list, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields(): void {
     this.name.set('');
     this.power.set(0);
   }
+
 }
